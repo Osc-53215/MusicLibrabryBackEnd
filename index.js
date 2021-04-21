5bef1eac5dd1ebbe588c0085dfbb7bfc09785c07
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-04-21 18:41:53 -0500

    added GET commands for reciving the whole music library as well as GET for especific id

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ const app = express();
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
-    
+
+//GET request for whole music library
+
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+   });
+
+
+
+// GET request through id
+
 app.get('/api/songs/:id', (req, res) => {
     const id = req.params.id;
-    const songs = repoContext.songs.findAllSongs();
+    const songs = repoContext.songs.findSongById(id);
     return res.send(songs);
    });
    
